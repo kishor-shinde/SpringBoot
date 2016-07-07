30d3aacfa91eea99e347d6a99ca1a2c6821f2d79
arpit107 <arpit.29082011@gmail.com>
2016-07-07 12:44:56 +0530

    Employee Details Added

--- src/main/java/com/peoplefy/GreetingController.java
+++ src/main/java/com/peoplefy/GreetingController.java
@@ -1,16 +1,37 @@
 package com.peoplefy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.peoplefy.domain.entity.EmployeeDetails;
+
+@RestController
 public class GreetingController {
 
     @RequestMapping("/greeting")
-    public @ResponseBody String greeting(@RequestParam("name") String name) {
+    public String greeting(@RequestParam("name") String name) {
         return "Hello " + name;
     }
 
+    @RequestMapping("/employees")
+    public List<EmployeeDetails> listEmployeea()
+    {
+    	ArrayList<EmployeeDetails> list=new ArrayList<EmployeeDetails>();
+    	EmployeeDetails ed=new EmployeeDetails();
+    	ed.setEmail("employee1@gmail.com");
+    	ed.setName("Employee1");
+    	ed.setId(1);
+    	list.add(ed);
+    	ed.setEmail("employee2@gmail.com");
+    	ed.setName("Employee2");
+    	ed.setId(2);
+    	list.add(ed);
+    	return list;
+    }
 }
